39b0a1bd8a3ae7bc0a4340d9de8777544ed45be5
N.M. Frolov <nmfrolov@icloud.com>
2026-03-12 12:05:13 +0100

    CompressHTML

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
   integrations: [mdx(), sitemap({
     filter: (page) => !page.includes('/thank-you') && !page.includes('/admin'),
   })],
-
+  compressHTML: true,
   vite: {
     plugins: [tailwindcss()],
   },
